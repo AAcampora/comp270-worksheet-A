6a8c1562ab01436c762ff87b1cc67766d97b0fd3
aacampora <acampora199@gmail.comn>
2019-10-15 15:54:16 +0100

    road compleate

--- Bezier.cpp
+++ Bezier.cpp
@@ -9,22 +9,22 @@ Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Ve
 void Bezier::draw(SDL_Renderer *renderer) const
 {
 	Vector2 newParamB;
+	Vector2 oldParamB = p0;
 
 	
 
 	for (size_t i = 0; i < 20; i++)
 	{
-		float t = i / 20;
+		float t = (i + 1.0f) / 20.0f;
+		float tt = (1.0f - t);
 
-		float tt = (1 - t);
-		newParamB.x = pow(tt, 3) * p0.x + 3 * (tt, 2) * t * p1.x + 3 * tt * (t, 2) * p2.x + (t, 3) * p3.x;
-		newParamB.y = pow(tt, 3) * p0.y + 3 * (tt, 2) * t * p1.y + 3 * tt * (t, 2) * p2.y + (t, 3) * p3.y;
+		newParamB.x = pow(tt, 3) * p0.x + 3 * pow(tt, 2) * t * p1.x + 3 * tt *
+			pow(t, 2) * p2.x + pow(t, 3) * p3.x;
+		newParamB.y = pow(tt, 3) * p0.y + 3 * pow(tt, 2) * t * p1.y + 3 * tt * 
+			pow(t, 2) * p2.y + pow(t, 3) * p3.y;
 		
-		SDL_RenderDrawLine(renderer, p0.x + newParamB.x, p0.y + newParamB.y, p1.x + newParamB.x, p1.y + newParamB.y);
-		SDL_RenderDrawLine(renderer, p1.x + newParamB.x, p1.y + newParamB.y, p2.x + newParamB.x, p2.y + newParamB.y);
-		SDL_RenderDrawLine(renderer, p2.x + newParamB.x, p2.y + newParamB.y, p3.x + newParamB.x, p3.y + newParamB.y); 
-		//DL_RenderDrawLine(renderer, p0.x , p0.y, p1.x, p1.y);
-		/*SDL_RenderDrawLine(renderer, p1.x, p1.y, p2.x, p2.y);
-		SDL_RenderDrawLine(renderer, p2.x, p2.y, p3.x, p3.y);*/
+		SDL_RenderDrawLine(renderer, newParamB.x, newParamB.y,oldParamB.x, oldParamB.y);
+
+		oldParamB = newParamB;
 	}
 }
