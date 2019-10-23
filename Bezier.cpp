47b9ab0b1af2858d87fea8fcf51b95926b95beca
alessio <acampor199@gmail.com>
2019-10-23 17:33:03 +0100

    finished part 2

--- Bezier.cpp
+++ Bezier.cpp
@@ -31,17 +31,17 @@ Vector2 Bezier::BezierCalulation(float t)const
 	Vector2 newParamB = Vector2 (); //the new calculated parm 
 	Vector2 oldParamB = p0; //setted to a new position at the end of every iteration
 
-		float tt = (1.0f - t); //tt is the reduction operator
-		float pt = t * t; // pt is t by the power of 2 
-		float ppt = pt * t; //ppt is t by the power of 3 
-		float ptt = tt * tt; //ppt is tt by the power of 2
-		float pptt = ptt * tt; //pptt is tt by the power of 3 
+		float tReduct = (1.0f - t); //tt is the reduction operator
+		float powT = t * t; // pt is t by the power of 2 
+		float pow3T = powT * t; //ppt is t by the power of 3 
+		float powTT = tReduct * tReduct; //ppt is tt by the power of 2
+		float pow3TT = powTT * tReduct; //pptt is tt by the power of 3 
 		//repeat this operation this number of times in order to render it a full track
 
 
 		//Cubic Bezier formula 
-		newParamB.x = pptt * p0.x + 3 * ptt * t * p1.x + 3 * tt * pt * p2.x + ppt * p3.x;
-		newParamB.y = pptt * p0.y + 3 * ptt * t * p1.y + 3 * tt * pt * p2.y + ppt * p3.y;
+		newParamB.x = pow3TT * p0.x + 3 * powTT * t * p1.x + 3 * tReduct * powT * p2.x + pow3T * p3.x;
+		newParamB.y = pow3TT * p0.y + 3 * powTT * t * p1.y + 3 * tReduct * powT * p2.y + pow3T * p3.y;
 
 		
 
